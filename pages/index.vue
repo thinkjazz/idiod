84075681c79a6415614b16aff83de6c5f688c102
lookrecius <bragin.boris@ya.ru>
2020-03-02 01:41:32 +0300

    Changed style and loop in section post into pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,60 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        blog
-      </h1>
-      <h2 class="subtitle">
-        idiod blog v.2.0
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <section id="posts">
+    <PostPreview
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnailImage="post.thumbUrl"
+      :id="post.id"
+    />
+  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import PostPreview from '@/components/Blog/PostPreview'
 export default {
   components: {
-    Logo
+    PostPreview
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: 'Пост про лисички',
+          previewText: 'Это текст для превьюхи c лисичками',
+          thumbUrl: 'https://i.pinimg.com/originals/0f/fd/dd/0ffddd32f693c21bebdacda949d231be.jpg',
+          id: 'a-foxy-post'
+        },
+        {
+          title: 'Пост про диоды',
+          previewText: 'Это текст для превьюхи c лисичками',
+          thumbUrl: 'https://i.pinimg.com/originals/0f/fd/dd/0ffddd32f693c21bebdacda949d231be.jpg',
+          id: 'a-diod-post'
+        },
+        {
+          title: 'Пост про диоды',
+          previewText: 'Это текст для превьюхи c лисичками',
+          thumbUrl: 'https://i.pinimg.com/originals/0f/fd/dd/0ffddd32f693c21bebdacda949d231be.jpg',
+          id: 'a-diod-post'
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style scoped>
+#posts {
+  padding-top: 2rem;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
-  text-align: center;
+  flex-direction: column;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
 }
 </style>
