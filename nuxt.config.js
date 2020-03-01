e170c98bb67e6b7f909d5ea5fbfce42ff9535c2f
lookrecius <bragin.boris@ya.ru>
2020-03-02 00:08:18 +0300

    Added font rel in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600&display=swap' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:100,300,400,600&display=swap' }
 
     ]
   },
