a1b189d2b4b6669efb64f1c1ef1d554872015e9c
lookrecius <bragin.boris@ya.ru>
2020-03-02 00:07:28 +0300

    Added some style for nav-links into layout/default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
+    <Header />
     <main class="main-content">
       <nuxt />
     </main>
-
   </div>
 </template>
-
+<script>
+import Header from '@/components/Header/Header'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
 <style>
 * {
   box-sizing: border-box;
